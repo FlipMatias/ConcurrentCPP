e156189f1b479b4e187e04fddba51fbf579dd1ac
FlipMatias <69702262+FlipMatias@users.noreply.github.com>
2020-09-24 08:40:33 -0300

    Create 1.2 pause_thread.cpp

---
+++ src/1.2 pause_thread.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+using namespace std::chrono_literals;
+
+#ifdef WIN32
+    #include <windows.h>
+#endif
+
+
+static bool isRunning = true;
+
+
+void chapolin()
+{
+    printf("| Chapolin thread's id is: %d\n", std::this_thread::get_id());
+
+    while (isRunning) {
+        printf("| Não contavam com a minha astúcia!! :)\n");
+        std::this_thread::sleep_for(2s);
+    }
+}
+
+
+void chaves()
+{
+    printf("| Chaves thread's id is: %d\n", std::this_thread::get_id());
+
+    while (isRunning) {
+        printf("| Foi sem querer queendo... :(\n");
+        std::this_thread::sleep_for(2s);
+    }
+}
+
+
+int main()
+{
+    #ifdef WIN32
+        SetConsoleOutputCP(CP_UTF8);
+    #endif
+
+    std::thread chapolinThread {chapolin};
+    std::thread chavesThread {chaves};
+
+    getchar();
+    isRunning = false;
+
+    chapolinThread.join();
+    chavesThread.join();
+
+    return 0;
+}
