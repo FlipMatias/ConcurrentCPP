ce4100c2bc6d423e778b09656881cf973ca203dd
FlipMatias <69702262+FlipMatias@users.noreply.github.com>
2020-09-25 00:28:30 -0300

    Create 1.3 pthreads.cpp

---
+++ src/1.3 pthreads.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <pthread.h>
+
+
+void* helloMamma(void* thread)
+{
+    long tId = *(long*)thread;
+    printf("| Game over! It's me, thred #%ld\n", tId);
+    pthread_exit(nullptr);
+}
+
+
+int main()
+{
+    constexpr int Num_Threads {2};
+    pthread_t threads[Num_Threads];
+
+    int rc;
+
+    for (long t = 0; t < Num_Threads; ++t)
+    {
+        printf("| In main: creating thread %ld\n", t);
+
+        rc = pthread_create(&threads[t], NULL, helloMamma, &t);
+        if (rc) {
+            printf("| Failed to create thread #%ld\n", t);
+            exit(-1);
+        }
+    }
+
+    pthread_exit(nullptr);
+    return 0;
+}
